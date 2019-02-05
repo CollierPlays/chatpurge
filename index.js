11750961dd1d84a9935e478df9ef3392157eedce
CollierPlays <collier.bell20@gmail.com>
2019-02-05 11:22:52 -0600

    Update index.js
    Removed the login message and made some comments for easier understanding.

--- index.js
+++ index.js
@@ -1,33 +1,31 @@
-let Discord = require("discord.js");
-let client = new Discord.Client();
-let config = require("./config.json");
-let package = require("./package.json");
+let Discord = require("discord.js");  // Imports the Discord.js npm package.
+let client = new Discord.Client();  // Declares the client
+let config = require("./config.json");  // Imports the config file
 
-console.log(`Logged in as ${client.user.tag} With version ${package.version}`);
-client.on("message", message => {
-  if (message.content !== config.command) return;
+client.on("message", message => { // Listens for a message
+  if (message.content !== config.command) return; // Checks if the message is the set command.
   let user = message.mentions.users.first();
   // Parse Amount
-  let amount = 9999999
+  let amount = 9999999 // How many messages to delete, that should be enough..
     ? parseInt(message.content.split(" ")[1])
     : parseInt(message.content.split(" ")[2]);
 
-  message.channel
-    .fetchMessages({
+  message.channel 
+    .fetchMessages({  // Gets the messages.
       limit: 100
     })
     .then(messages => {
       if (user) {
-        let filterBy = user ? user.id : client.user.id;
+        let filter = user ? user.id : client.user.id; 
         messages = messages
-          .filter(m => m.author.id === filterBy)
+          .filter(m => m.author.id === filter)
           .array()
           .slice(0, amount);
       }
       message.channel
-        .bulkDelete(messages)
-        .catch(error => console.log(error.stack));
+        .bulkDelete(messages) // Purge the messages
+        .catch(error => console.log(error.stack));  // Catch and log any errors that occur.
     });
 });
 
-client.login(config.token);
+client.login(config.token); // Login
